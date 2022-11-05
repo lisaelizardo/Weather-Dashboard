58260610b024dcffcfe7e6dee6d65c09b8463445
Deneene Whitaker <lisaelizrdo@gmail.com>
2022-11-05 11:23:39 -0500

    second push

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=47442d983b570f5f60f2fd483ddea261')
-.then(function(response){
-    return response.json();
-})
-.then(function(data){
-    console.log(data);
-});
+var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=47442d983b570f5f60f2fd483ddea261'
+var searchButton = document.getElementById('button');
+var searchInput = document.getElementById('search-input');
+var today = moment();
+var currentTime = moment().hour();
 
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+
+searchButton.addEventListener("click")
+    
+fetch(requestUrl) {
+    .then(function (response){
+    if (response.status === 200)
+    return response.json();
+}
+    .then(function(data){
+    console.log(data);
+})
+}
+
+
+//fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=47442d983b570f5f60f2fd483ddea261')
+//.then(function(response){
+//    return response.json();
+//})
+//.then(function(data){
+//    console.log(data);
+//});
