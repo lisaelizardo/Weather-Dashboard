0b9159338acfffbec800476828f8c489ef696f1c
Deneene Whitaker <lisaelizrdo@gmail.com>
2022-11-07 22:24:37 -0600

    changing fetch code

--- script.js
+++ script.js
@@ -3,21 +3,20 @@ var searchButton = document.getElementById('button');
 var searchInput = document.getElementById('search-input');
 var today = moment();
 var currentTime = moment().hour();
+var weather = document.getElementById('weather');
 
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
 searchButton.addEventListener("click")
-    
-fetch(requestUrl) {
-    .then(function (response){
-    if (response.status === 200)
-    return response.json();
-}
-    .then(function(data){
-    console.log(data);
-})
-}
 
+
+fetch(requestUrl)
+    .then(function (response) => {
+        response.json()
+    .then(function (data) => {
+        weather(data);
+    })
+    });
 
 //fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=47442d983b570f5f60f2fd483ddea261')
 //.then(function(response){
